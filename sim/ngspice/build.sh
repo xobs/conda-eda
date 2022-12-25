21d1643263d89cb0b6939b8d28172ab7dd6dc0c2
Johan Euphrosine <proppy@google.com>
2022-12-25 11:14:23 +0900

    sim/ngspice: enable more flags

--- sim/ngspice/build.sh
+++ sim/ngspice/build.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 ./autogen.sh
-./configure --prefix="${PREFIX}" --with-x --enable-xspice --enable-cider --with-readline=yes --enable-openmp --disable-debug
+./configure --prefix="${PREFIX}" --with-x --enable-xspice --enable-cider --with-readline=yes --enable-openmp --enable-pss --disable-debug
 make V=1 -j$CPU_COUNT
 make V=1 install
