e3d2f2acd7d69637cd81a8e59621913cfed60208
Johan Euphrosine <proppy@google.com>
2022-12-25 12:16:35 +0900

    installer/analog: add launchers post_install

--- installer/analog.sky130a/post_install.sh
+++ installer/analog.sky130a/post_install.sh
@@ -58,6 +58,27 @@ diff -Nru analog-env-1/share/pdk/sky130A/libs.tech/klayout/tech/sky130A.lyt anal
    <gds2>
 EOF
 
+cat > $PREFIX/etc/conda/activate.d/open_pdks_activate.sh <<EOF
+export PDK_ROOT=\$CONDA_PREFIX/share/pdk
+EOF
+
 cat > $PREFIX/etc/conda/activate.d/klayout_activate.sh <<EOF
 export KLAYOUT_HOME=\$CONDA_PREFIX/share/pdk/sky130A/libs.tech/klayout
 EOF
+
+cat > $PREFIX/etc/conda/activate.d/ngspice_activate.sh <<EOF
+export SPICE_USERINIT_DIR=\$CONDA_PREFIX/share/pdk/sky130A/libs.tech/ngspice
+(cd \$CONDA_PREFIX/share/pdk/sky130A/libs.tech/ngspice && ln spinit .spiceinit)
+EOF
+
+mv $PREFIX/bin/magic $PREFIX/bin/magic.real
+cat > $PREFIX/bin/magic <<EOF
+magic.real -rcfile \$CONDA_PREFIX/share/pdk/sky130A/libs.tech/magic/sky130A.magicrc 
+EOF
+chmod +x $PREFIX/bin/magic
+
+mv $PREFIX/bin/xschem $PREFIX/bin/xschem.real
+cat > $PREFIX/bin/xschem <<EOF
+xschem.real --rcfile \$CONDA_PREFIX/share/pdk/sky130A/libs.tech/xschem/xschemrc
+EOF
+chmod +x $PREFIX/bin/xschem
