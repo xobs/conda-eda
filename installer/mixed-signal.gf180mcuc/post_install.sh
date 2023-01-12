1e02e1719b794f86fb5a9ca6a70aeb1b27dc6231
Johan Euphrosine <proppy@google.com>
2023-01-12 23:57:56 +0900

    install/gf180mcu: fix variable escape

--- installer/mixed-signal.gf180mcuc/post_install.sh
+++ installer/mixed-signal.gf180mcuc/post_install.sh
@@ -8,18 +8,18 @@ export PDK=gf180mcuC
 EOF
 
 cat > $PREFIX/etc/conda/activate.d/klayout_activate.sh <<EOF
-export KLAYOUT_HOME=\$CONDA_PREFIX/share/pdk/\$PDK/libs.tech/klayout
+export KLAYOUT_HOME=\$CONDA_PREFIX/share/pdk/$\PDK/libs.tech/klayout
 EOF
 
 mv $PREFIX/bin/magic $PREFIX/bin/magic.real
 cat > $PREFIX/bin/magic <<EOF
-magic.real -rcfile \$CONDA_PREFIX/share/pdk/$\PDK/libs.tech/magic/\$PDK.magicrc \$@
+magic.real -rcfile \$CONDA_PREFIX/share/pdk/\$PDK/libs.tech/magic/\$PDK.magicrc \$@
 EOF
 chmod +x $PREFIX/bin/magic
 
 mv $PREFIX/bin/xschem $PREFIX/bin/xschem.real
 cat > $PREFIX/bin/xschem <<EOF
-xschem.real --rcfile \$CONDA_PREFIX/share/pdk/$\PDK/libs.tech/xschem/xschemrc \$@
+xschem.real --rcfile \$CONDA_PREFIX/share/pdk/\$PDK/libs.tech/xschem/xschemrc \$@
 EOF
 chmod +x $PREFIX/bin/xschem
 
