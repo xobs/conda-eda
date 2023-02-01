b0b04de9e963554c6d4234718847a134f41699ea
Johan Euphrosine <proppy@google.com>
2023-02-01 12:22:39 +0900

    mixed-signal.gf180mcuc: fix xschem model path

--- installer/mixed-signal.gf180mcuc/post_install.sh
+++ installer/mixed-signal.gf180mcuc/post_install.sh
@@ -26,6 +26,7 @@ cat >> $PREFIX/share/pdk/gf180mcuC/libs.tech/xschem/xschemrc <<EOF
 set XSCHEM_START_WINDOW \${PDK_ROOT}/gf180mcuC/libs.tech/xschem/tests/0_top.sch
 append XSCHEM_LIBRARY_PATH :\${PDK_ROOT}/gf180mcuC/libs.tech/xschem
 EOF
+sed -i -e 's/ngspice\/models/gf180mcuC\/libs.tech\/ngspice/' $PREFIX/share/pdk/gf180mcuC/libs.tech/xschem/xschemrc 
 
 # fix up yosys dep
 (cd $PREFIX/lib && ln -s libffi.so.7 libffi.so.6)
