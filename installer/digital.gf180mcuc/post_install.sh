3c9db2718c480f47e62c63e3ee8bc590d74b6c9d
Johan Euphrosine <proppy@google.com>
2023-02-03 21:14:46 +0900

    install/digital.gf180mcuc: fix stdlib

--- installer/digital.gf180mcuc/post_install.sh
+++ installer/digital.gf180mcuc/post_install.sh
@@ -4,8 +4,8 @@ set -e
 # set gf180mcuc defaults
 cat >> $PREFIX/share/openlane/install/env.tcl <<EOF
 set ::env(PDK) "gf180mcuC"
-set ::env(STD_CELL_LIBRARY) "sky130_fd_sc_hd"
-set ::env(STD_CELL_LIBRARY_OPT) "sky130_fd_sc_hd"
+set ::env(STD_CELL_LIBRARY) "gf180mcu_fd_sc_mcu7t5v0"
+set ::env(STD_CELL_LIBRARY_OPT) "gf180mcu_fd_sc_mcu7t5v0"
 EOF
 
 # fix up yosys dep
